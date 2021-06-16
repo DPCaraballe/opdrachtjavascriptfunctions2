ae9957f08f31771cc567bbe8bc3621f4a2d11707
DPCaraballe <84527568+DPCaraballe@users.noreply.github.com>
2021-06-16 13:32:49 +0200

    Add files via upload

---
+++ opdrachtjavascriptfunctions2/functions.js
@@ -0,0 +1,10 @@
+ 
+const paintObject = function(object, color) {
+    console.log("Paint" + " " + object  + " " + color);
+    console.log("The" + " " + object + " " + "has been painted"  + " " + color);
+
+};
+
+
+paintObject("wall", "purple");
+
